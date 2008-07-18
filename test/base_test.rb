0abddf1c41d2475221d362ed6a71be610ad14f1e
James Golick <james@james-mbp.local>
2008-07-18 13:57:33 -0400

    setup some test infrastructure

---
+++ test/base_test.rb
@@ -0,0 +1,5 @@
+require File.dirname(__FILE__)+'/test_helper'
+
+class BaseTest < Test::Unit::TestCase
+  
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,24 @@
-require 'rubygems'
-require 'activerecord'
+require File.dirname(__FILE__)+'/../lib/active_presenter'
 require 'test/unit'
+require 'mocha'
+
+ActiveRecord::Base.configurations = {'sqlite3' => {:adapter => 'sqlite3', :database => ':memory:'}}
+ActiveRecord::Base.establish_connection('sqlite3')
+
+ActiveRecord::Schema.define(:version => 0) do
+  create_table :users do |t|
+    t.string :login,    :default => ''
+    t.string :password, :default => ''
+  end
+  
+  create_table :accounts do |t|
+    t.string :subdomain, :default => ''
+  end
+end
+
+class User < ActiveRecord::Base; end
+class Account < ActiveRecord::Base; end
+
+class SignupPresenter
+  
+end
