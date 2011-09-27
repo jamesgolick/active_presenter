3865398ceb7ab81e25f856631dc87f067c953595
James Golick <jamesgolick@gmail.com>
2011-09-26 17:16:41 -0700

    2.0.0a

--- lib/active_presenter/version.rb
+++ lib/active_presenter/version.rb
@@ -1,9 +1,10 @@
 module ActivePresenter
   module VERSION
-    MAJOR    = 1
-    MINOR    = 4
+    MAJOR    = 2
+    MINOR    = 0
     TINY     = 0
+    EXTRA    = "a"
 
-    STRING   = [MAJOR, MINOR, TINY].join('.')
+    STRING   = [MAJOR, MINOR, TINY].join('.') + EXTRA
   end
 end
